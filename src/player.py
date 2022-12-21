3ab4b4f8932d7c4d273c0bef840ca2965cffd7e9
Jose Salcido <jmsalcidoaguilar@gmail.com>
2022-12-21 15:42:45 -0700

    basic animations

--- src/player.py
+++ src/player.py
@@ -29,21 +29,39 @@ class Player(pygame.sprite.Sprite):
             full_path = '../graphics/character/' + animation
             self.animations[animation] = import_folder(full_path)
 
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
+
+        self.image = self.animations[self.status][int(self.frame_index)]
+
     def input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
+            self.status = 'up'
             self.direction.y = -1
         elif keys[pygame.K_DOWN]:
+            self.status = 'down'
             self.direction.y = 1
         else:
             self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
+            self.status = 'left'
             self.direction.x = -1
         elif keys[pygame.K_RIGHT]:
+            self.status = 'right'
             self.direction.x = 1
         else:
             self.direction.x = 0
+
+    def get_status(self):
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split('_')[0] + '_idle'
+
+        # tool use
+        pass
 
     def move(self, dt):
         if self.direction.magnitude() > 0:
@@ -57,4 +75,6 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
