104b9599b01275abb4a4b7d2c5399ab3f79ab3e6
Jose Salcido <jmsalcidoaguilar@gmail.com>
2022-12-21 15:30:48 -0700

    entire setup

---
+++ src/player.py
@@ -0,0 +1,60 @@
+import pygame
+
+from support import import_folder
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group) -> None:
+        super().__init__(group)
+
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=pos)
+
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
+
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, dt):
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
---
+++ src/support.py
@@ -0,0 +1,14 @@
+from os import walk
+import pygame
+
+
+def import_folder(path):
+    surface_list = []
+
+    for _, __, img_files in walk(path):
+        for image in img_files:
+            full_path = f"{path}/{image}"
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+
+    return surface_list
